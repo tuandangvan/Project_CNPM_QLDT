6040a95b1a06de4ca2ccb23fc78fbd383de53942
Mai Bảo Huy <106733089+baohuyxxi@users.noreply.github.com>
2022-12-17 03:11:10 +0700

    huy push

--- Project_CNPM_QuanLyDeTai/src/main/java/Dao/Impl/PageDaoImpl.java
+++ Project_CNPM_QuanLyDeTai/src/main/java/Dao/Impl/PageDaoImpl.java
@@ -14,7 +14,8 @@ public class PageDaoImpl extends DBConnection implements IPageDao {
 	{
 		String sql = "DECLARE @value nvarchar(50)\r\n"
 				+ "set @value= N'%"+key+"%'\r\n"
-				+ "select COUNT ( Students.studentId)/10+1 as count\r\n"
+				+ "select COUNT  (distinct Students.studentId)/10+1 as count,\r\n"
+				+ "COUNT (distinct Students.studentId) as element\r\n"
 				+ "from Students, Majors\r\n"
 				+ "where Students.studentName like @value or Students.studentId  like @value \r\n"
 				+ "or Students.phone  like @value or majorName  like @value";
@@ -25,6 +26,7 @@ public class PageDaoImpl extends DBConnection implements IPageDao {
 			while (rs.next()) {
 				PageModel page = new PageModel();
 				page.setPage(rs.getInt("count"));
+				page.setElement(rs.getInt("element"));
 				return page;
 			}
 		} catch (Exception e) {
@@ -37,7 +39,8 @@ public class PageDaoImpl extends DBConnection implements IPageDao {
 	{
 		String sql = "DECLARE @value nvarchar(50)\r\n"
 				+ "set @value= N'%"+key+"%'\r\n"
-				+ "select COUNT ( Teachers.teacherId)/10+1 as count\r\n"
+				+ "select COUNT ( distinct Teachers.teacherId)/10+1 as count\r\n"
+				+ "COUNT (distinct Teachers.teacherId) as element\r\n"
 				+ "from Teachers\r\n"
 				+ "where Teachers.teacherName like @value \r\n"
 				+ "or Teachers.teacherId like @value or Teachers.phone  like @value\r\n"
@@ -49,6 +52,7 @@ public class PageDaoImpl extends DBConnection implements IPageDao {
 			while (rs.next()) {
 				PageModel page = new PageModel();
 				page.setPage(rs.getInt("count"));
+				page.setElement(rs.getInt("element"));
 				return page;
 			}
 		} catch (Exception e) {
@@ -60,7 +64,8 @@ public class PageDaoImpl extends DBConnection implements IPageDao {
 	{
 		String sql = "DECLARE @value nvarchar(50)\r\n"
 				+ "set @value= N'%CNTT%'\r\n"
-				+ "select COUNT ( Topic.topicName)/10+1 as count\r\n"
+				+ "select COUNT (distinct Topic.topicId)/10+1 as count\r\n"
+				+ "COUNT (distinct Topic.topicId) as element\r\n"
 				+ "from Topic, Teachers\r\n"
 				+ "where Topic.topicName like @value or teacherName like @value";
 		try {
@@ -70,6 +75,7 @@ public class PageDaoImpl extends DBConnection implements IPageDao {
 			while (rs.next()) {
 				PageModel page = new PageModel();
 				page.setPage(rs.getInt("count"));
+				page.setElement(rs.getInt("element"));
 				return page;
 			}
 		} catch (Exception e) {
